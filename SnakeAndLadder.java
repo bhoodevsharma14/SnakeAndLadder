93391412b492c73bdcdb25239761087e9df2ac65
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-13 15:38:38 +0530

    Added: Repeating Untill reach 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,19 +8,27 @@ public class SnakeAndLadder {
 		System.out.println("Welcome to Snake And Ladder Simulator");
 		
 		int player1Position=0;
-
-		int dice= (int) Math.floor(Math.random()*10)%6 +1;
-
-		int move= (int) Math.floor(Math.random()*10)%3;
 		
-		switch (move)
+		while(player1Position<100)
 		{
-			case LADDER: 
-						player1Position+=dice;
-						break;
-			case SNAKE:
-						player1Position-=dice;
-						break;		
+			
+			int dice= (int) Math.floor(Math.random()*10)%6 +1;
+			
+			int move= (int) Math.floor(Math.random()*10)%3;
+			
+			switch (move)
+			{
+				case LADDER: 
+							player1Position+=dice;
+							break;
+				case SNAKE:
+							player1Position-=dice;
+							break;		
+			}
+//			System.out.println(" dice "+dice+" ,move "+move+" position: "+player1Position);
+			if (player1Position<0)
+				player1Position=0;
+			System.out.println(" dice "+dice+" ,move "+move+" position: "+player1Position);
 		}
 	}
 }
