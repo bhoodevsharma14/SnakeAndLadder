7c66bc6c087dbe817ebff717ddc8fec6bc50767d
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-13 13:06:48 +0530

    Added: Player Can Roll A Dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,6 +7,9 @@ public class SnakeAndLadder {
 		System.out.println("Welcome to Snake And Ladder Simulator");
 		
 		int player1Position=0;
-		
+
+		int dice= (int) Math.floor(Math.random()*10)%6 +1;
+
+
 	}
 }
