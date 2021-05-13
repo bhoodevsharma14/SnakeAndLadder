7a4f2fcec07cf55678201bb4352470ba018791ab
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-13 22:18:23 +0530

    Added: Two Players can Play The Game

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,38 +2,76 @@ package com.snakeandladder;
 
 public class SnakeAndLadder {
 	public static final int NO_MOVE=0,LADDER=1,SNAKE=2;
+	public static int player1Position=0,rollCounter=0,player2Position=0;
 
+	public static int chance=1;
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Snake And Ladder Simulator");
 		
-		int player1Position=0,rollCounter=0;
 		
-		while(player1Position<100)
+		while(player1Position<100 && player2Position<100)
 		{
-			
+			if (chance==1)
+			{
+				player1Position=rollDice(player1Position);
+				chance=2;
+				continue;
+			}
+			else
+			{
+				player2Position=rollDice(player2Position);
+				chance=1;
+				continue;
+			}
+		}
+		
+		if(player1Position==100)
+			System.out.println("Dice Rolled for : " +rollCounter+" times and Player 1 is the Winner");
+		if(player2Position==100)
+			System.out.println("Dice Rolled for : " +rollCounter+" times and Player 2 is the Winner");
+		
+
+	}
+	
+	public static int rollDice(int playerPosition)
+	{
+		boolean condition=true;
+		while(playerPosition<100 && condition)
+		{
 			int dice= (int) Math.floor(Math.random()*10)%6 +1;
 			
 			int move= (int) Math.floor(Math.random()*10)%3;
 			
 			rollCounter++;
-			
+
 			switch (move)
 			{
 				case LADDER: 
-							player1Position+=dice;
-							if(player1Position>100)
-								player1Position-=dice;
+							playerPosition+=dice;
+							if(playerPosition>100)
+								{
+									playerPosition-=dice;
+									condition=false;
+								}
+								
 							break;
 				case SNAKE:
-							player1Position-=dice;
+							playerPosition-=dice;
+							condition=false;
 							break;		
+				default:
+						condition=false;
 			}
 			
-			if (player1Position<0)
-				player1Position=0;
+			if (playerPosition<0)
+				playerPosition=0;
 			
-			System.out.println("Dice Rolled for : " +rollCounter+" Player 1 Position is : "+player1Position);
 		}
+			
+		System.out.println("Dice Rolled for : " +rollCounter+" Player "+chance+" Position is : "+playerPosition);
+		return playerPosition;
+		
+		
 	}
 }
